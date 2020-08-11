678c697441b60bcdf803a7e67e451710d9670b2c
Muhammad Faisal <muhfaisal90@hotmail.com>
2020-08-11 12:00:14 +0500

    test push for gcloud

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel On Google Cloud
                 </div>
 
                 <div class="links">
